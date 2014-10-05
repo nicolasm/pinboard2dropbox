df6e31c597ab3128a127b30d30cdaffadf862eba
nicolasm <ws@nclsmr.com>
2014-10-05 18:22:01 +0200

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,4 @@
+#!/bin/bash -
+cp backup_bookmarks.sh ~/Library/Scripts/Applications/Pinboard/
+cp get_netrc.py ~/Library/Scripts/Applications/
+cp openssl_encrypt.sh ~/Library/Scripts/Applications/
