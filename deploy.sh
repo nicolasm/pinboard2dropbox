9566987c2b25267f643635fbbd7752c73b0d7d08
nicolasm <ws@nclsmr.com>
2014-10-05 18:47:59 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,2 @@
 #!/bin/bash -
 cp backup_bookmarks.sh ~/Library/Scripts/Applications/Pinboard/
-cp get_netrc.py ~/Library/Scripts/Applications/
-cp openssl_encrypt.sh ~/Library/Scripts/Applications/
