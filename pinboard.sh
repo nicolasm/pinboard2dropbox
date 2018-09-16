f8b2839f7bbbcf4ce2f797b1c4878559f9043a71
Nicolas Meier <ws@nclsmr.com>
2018-09-16 15:56:41 +0200

    Put results in json objects

--- pinboard.sh
+++ pinboard.sh
@@ -1,2 +1,2 @@
 #!/bin/bash -
-jq ".[] | select((.description | contains(\"$1\")) or (.tags | contains(\"$1\")) or (.href | contains(\"$1\")) or (.extended | contains(\"$1\"))) | .description, .href" ~/Dropbox/Backups/Pinboard/Pinboard.json
+jq ".[] | select((.description | contains(\"$1\")) or (.tags | contains(\"$1\")) or (.href | contains(\"$1\")) or (.extended | contains(\"$1\"))) | {description: .description, href: .href}" ~/Dropbox/Backups/Pinboard/Pinboard.json
